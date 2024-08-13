c09651c7deec13674b10f6b9a20c5836d335f19e
minjeongJe <zzssjj@naver.com>
2024-08-13 14:24:38 +0900

    second commit

--- src/component/Box.js
+++ src/component/Box.js
@@ -1,14 +1,25 @@
+//box.js
 import React from 'react'
 
 const Box = (props) => {
     console.log("props",props);
+    let result;
+    if(
+    props.title === "Computer" && 
+    props.result !== "Tie" && 
+    props.result !== "") {
+        result = props.result === "Win" ? "Lose" : "Win"; 
+    }else {
+        result = props.result;
+    }
+
   return (
-    <div className='box'>
+    <div className = {`box ${result}`}>
         <h1>{props.title}</h1>
-        <img className='item-img' src={props.item && props.item.img}/>
-        <h2>WIN</h2>
+        <img className='item-img' src={props.item &&  props.item.img}/>
+        <h2>{result}</h2>
     </div>
-  )
+  );
 }
 
 export default Box
